e5677e5f69a64526c248ffe4f31ed0e2718356c2
aliaxei <touch.byy@gmail.com>
2019-05-01 00:51:21 +0300

    delete all info from testActivity

--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -17,61 +17,13 @@ import com.example.myapplication.requestEngine.InputUserInfoActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestActivity extends AppCompatActivity implements View.OnTouchListener {
+public class TestActivity extends AppCompatActivity  {
 
-    Button requestButton;
-    Animation animScale;
-    String font;
-    WorkWithFile workWithFile;
-    List<TextView> textViewsList = new ArrayList<>();
-    TextView textView,textView1,textViewSearchUserButton,textViewSearchButton,textViewRequestButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        workWithFile = new WorkWithFile();
-        requestButton = findViewById(R.id.requestButton);
-        requestButton.setOnTouchListener(this);
-        animScale = AnimationUtils.loadAnimation(this, R.anim.alpha_animation);
-        textView = findViewById(R.id.textView2);
-        textViewsList.add(textView);
-        textView1 = findViewById(R.id.textView3);
-        textViewsList.add(textView1);
-        textViewSearchUserButton = findViewById(R.id.textViewSearchUserButton);
-        textViewsList.add(textViewSearchUserButton);
-        textViewSearchButton = findViewById(R.id.textViewSearchButton);
-        textViewsList.add(textViewSearchButton);
-        textViewRequestButton = findViewById(R.id.textViewRequestButton);
-        textViewsList.add(textViewRequestButton);
 
-
-
-
-
-        font = workWithFile.ReadFromFile(workWithFile.getFontsFileName(),getApplicationContext());
-        if (font != null) {
-            FontChanger.changeTheFont(font,textViewsList,this);
-        }
-
-
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (v.getId()){
-            case R.id.requestButton:
-                switch (event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        v.startAnimation(animScale);
-                        break;
-                        case MotionEvent.ACTION_UP:
-                            Intent intent = new Intent(this, InputUserInfoActivity.class);
-                            startActivity(intent);
-                            break;
-                }
-                break;
-        }
-        return true;
     }
 }
